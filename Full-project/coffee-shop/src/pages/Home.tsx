9eaaef4eb498525e28046e8b5d86b89784d0a7c0
Yunnna005 <annakovalenko0500@gmail.com>
2025-10-06 16:38:04 +0000

    Implement navigation and add order functionality with routing

---
+++ Full-project/coffee-shop/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hero from "../layout/Hero";
+import Menu from "../layout/Menu";
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <Menu />
+        </>
+    );
+}
