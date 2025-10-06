0b1c8262acc4fc68e1e20cf0b9ac7ca4827d5e04
Yunnna005 <annakovalenko0500@gmail.com>
2025-10-06 15:29:45 +0000

    Implemented toast

--- Full-project/coffee-shop/src/App.tsx
+++ Full-project/coffee-shop/src/App.tsx
@@ -3,20 +3,26 @@ import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Menu from './components/Menu'
 import NavBar from './components/NavBar'
+import Toast from './components/Toast';
 
 
 function App() {
   const [orderQuantity, setOrderQuantity] = useState(0);
+  const [toastMessage, setToastMessage] = useState("");
 
   const handleAddToOrder = () => {
     setOrderQuantity((prev: number) => prev + 1);
+    setToastMessage("Item was added to the order.");
   };
+
+  const handleToastClose = () => setToastMessage("");
 
   return (
     <>
       <NavBar orderQuantity={orderQuantity}/>
       <Hero />
       <Menu onAddToOrder={handleAddToOrder}/>
+      {toastMessage && <Toast onClose={handleToastClose} />}
       <Footer />
     </>
   )
