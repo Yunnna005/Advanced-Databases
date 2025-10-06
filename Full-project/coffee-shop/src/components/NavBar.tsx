c8faf4a334ad53b64f7ec4d8c1e5e8beffce39f9
Yunnna005 <annakovalenko0500@gmail.com>
2025-10-06 14:46:14 +0000

    Update navbar

--- Full-project/coffee-shop/src/components/NavBar.tsx
+++ Full-project/coffee-shop/src/components/NavBar.tsx
@@ -2,10 +2,10 @@ export default function NavBar() {
     return (
         <nav className="bg-white fixed top-0 left-0 right-0 border-b border-stone-800 dark:bg-stone-800 z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <div className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="coffee.svg" className="h-8" alt="Coffee Shop Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Coffee Shop</span>
-            </a>
+            </div>
             <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
